bd0207d52f2831afc57d66c77509b2588f790bac
biu <104364996+biubiumaxxx@users.noreply.github.com>
2022-05-12 01:24:15 +0800

    Update reload.js

--- Surge/Module/Profile-Reload/reload.js
+++ Surge/Module/Profile-Reload/reload.js
@@ -4,6 +4,6 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
         title: "Profile Reload",
         content: "配置檔案重載成功",
         icon: "arrow.triangle.2.circlepath.doc.on.clipboard",
-        "icon-color": "#8B81C3",
+        "icon-color": "#005CAF",
      })
     });
